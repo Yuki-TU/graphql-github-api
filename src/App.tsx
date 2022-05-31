41f3d00776d5055d100debbdc9f42ab87df4395b
Yuki-TU <k.yuki.90219@icloud.com>
2022-06-01 03:24:11 +0900

    リファクタリング

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { useQuery } from "@apollo/client";
 import React from "react";
-import "./App.css";
 import { UserInfoDocument, UserInfoQuery } from "./generated";
 
 function App() {
   const { data, loading, error } = useQuery<UserInfoQuery>(UserInfoDocument);
+
+  if (loading) return <p>...loding</p>;
+  if (error) return <p>error</p>;
 
   return <p>{data?.viewer.login}</p>;
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,6 @@ import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
-import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -20,8 +18,3 @@ root.render(
     </ApolloProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
